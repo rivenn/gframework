71f35cd2836156e0b33061b8a48713a72fa02c32
Lirong Yuan <yuanzi@google.com>
2014-09-23 13:45:20 -0700

    Initial commit

---
+++ FrameworkA.framework/Headers/FrameworkA.h
@@ -0,0 +1,21 @@
+//
+//  FrameworkA.h
+//  FrameworkA
+//
+//  Created by Lirong Yuan on 9/23/14.
+//  Copyright (c) 2014 google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkA.
+FOUNDATION_EXPORT double FrameworkAVersionNumber = 1.0.0;
+
+//! Project version string for FrameworkA.
+FOUNDATION_EXPORT const unsigned char FrameworkAVersionString[] = @"1.0.0";
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkA/PublicHeader.h>
+
+#import <FrameworkA/GFWA.h>
+
+
---
+++ FrameworkC.framework/Headers/FrameworkC.h
@@ -0,0 +1,20 @@
+//
+//  FrameworkC.h
+//  FrameworkC
+//
+//  Created by Lirong Yuan on 9/23/14.
+//  Copyright (c) 2014 google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkC.
+FOUNDATION_EXPORT double FrameworkCVersionNumber = 1.0.0;
+
+//! Project version string for FrameworkC.
+FOUNDATION_EXPORT const unsigned char FrameworkCVersionString[] = @"1.0.0";
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkC/PublicHeader.h>
+
+#import <FrameworkC/GFWC.h>
+
