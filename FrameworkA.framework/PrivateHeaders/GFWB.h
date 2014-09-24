67f16a0dd619bde5ea4c8dda6b781e75948c5a76
Lirong Yuan <yuanzi@google.com>
2014-09-23 21:32:49 -0700

    Changes to be committed: 	deleted:    FrameworkA.framework/.DS_Store 	typechange: FrameworkA.framework/FrameworkA 	deleted:    FrameworkA.framework/Headers 	renamed:    FrameworkA.framework/Versions/A/Headers/FrameworkA.h -> FrameworkA.framework/Headers/FrameworkA.h 	renamed:    FrameworkA.framework/Versions/A/Headers/GFWA.h -> FrameworkA.framework/Headers/GFWA.h 	new file:   FrameworkA.framework/Info.plist 	new file:   FrameworkA.framework/Modules/module.modulemap 	new file:   FrameworkA.framework/PrivateHeaders/GFWB.h 	deleted:    FrameworkA.framework/Versions/A/FrameworkA 	deleted:    FrameworkA.framework/Versions/Current

---
+++ FrameworkA.framework/PrivateHeaders/GFWB.h
@@ -0,0 +1,15 @@
+//
+//  GFWB.h
+//  FrameworkA
+//
+//  Created by Lirong Yuan on 9/23/14.
+//  Copyright (c) 2014 google. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GFWB : NSObject
+
++ (NSString *)GFWBString;
+
+@end
