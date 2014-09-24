c437c2d647d511acf43386fd964992c96c838b52
Lirong Yuan <yuanzi@google.com>
2014-09-23 18:46:29 -0700

    Changes to be committed: 	new file:   FrameworkA.framework/Versions/.DS_Store 	typechange: FrameworkC.framework/FrameworkC 	new file:   FrameworkC.framework/Headers 	renamed:    FrameworkC.framework/FrameworkC -> FrameworkC.framework/Versions/A/FrameworkC 	renamed:    FrameworkC.framework/Headers/FrameworkC.h -> FrameworkC.framework/Versions/A/Headers/FrameworkC.h 	renamed:    FrameworkC.framework/Headers/GFWC.h -> FrameworkC.framework/Versions/A/Headers/GFWC.h 	new file:   FrameworkC.framework/Versions/Current 	new file:   genframework.sh

---
+++ genframework.sh
@@ -0,0 +1,11 @@
+cd $1.framework
+chmod 666 $1
+mkdir Versions
+cd Versions
+mkdir A
+mv ../$1 A
+mv ../Headers A/
+ln -s A Current
+cd ..
+ln -s Versions/Current/Headers Headers
+ln -s Versions/Current/$1 $1
