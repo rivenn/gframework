96ba2c1ffe3856cfc809906b371c66aa3c556dfc
Lirong Yuan <yuanzi@google.com>
2014-09-23 20:29:25 -0700

    Changes to be committed: 	new file:   MasterFramework.framework/.DS_Store 	new file:   MasterFramework.framework/Headers/GFWA.h 	new file:   MasterFramework.framework/Headers/GFWB.h 	new file:   MasterFramework.framework/Headers/MasterFramework.h 	new file:   MasterFramework.framework/Info.plist 	new file:   MasterFramework.framework/LICENSE 	new file:   MasterFramework.framework/MasterFramework 	new file:   MasterFramework.framework/Modules/module.modulemap 	new file:   MasterFramework.framework/PrivateHeaders/GFWC.h 	new file:   MasterFramework.framework/ResourceRules.plist 	new file:   MasterFramework.framework/_CodeSignature/CodeResources

---
+++ MasterFramework.framework/Headers/MasterFramework.h
@@ -0,0 +1,21 @@
+//
+//  MasterFramework.h
+//  MasterFramework
+//
+//  Created by Lirong Yuan on 9/23/14.
+//  Copyright (c) 2014 google. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double MasterFrameworkVersionNumber = 1.0;
+
+FOUNDATION_EXPORT const unsigned char MasterFrameworkVersionString[] = "1.0.0";
+
+// In this header, you should import all the public headers of your framework using statements like #import <MasterFramework/PublicHeader.h>
+
+
+#import <MasterFramework/GFWA.h>
+#import <MasterFramework/GFWC.h>
+
+
